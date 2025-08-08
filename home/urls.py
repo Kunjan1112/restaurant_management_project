37a96b01687f25bc7633ccf530b14378b44d9483
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-08 12:15:02 +0530

    25132

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('name/',views.name,name="name"),
-    path('index/',views.index,name="index"),
-    path('login/',views.login,name="login"),
-
+    path()
 ]
-
