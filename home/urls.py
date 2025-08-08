a1d802b834e9544147340ef7f5f24f58b7e5c396
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-08 12:16:55 +0530

    25132

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path()
+    path('name/',views.name),
+
+    path('index/',views.index,name='index'),
+
+    path('login_view/',views.login_view,name='login_view'),
+
+    path('404_view/',view.404_view,name='404_view'),
 ]
