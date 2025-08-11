6fe9f5bf3418ecca502b9aeb489a43417243482f
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-11 12:25:24 +0530

    25135

--- home/urls.py
+++ home/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login_view/',views.login_view,name='login_view'),
 
     path('404_view/',view.404_view,name='404_view'),
+
+    path('about_restaurant/',views.about_restaurant,name='about_restaurant'),
 ]
