73d239c573d1c4bf77a83f448d70f735aeb3f42e
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-12 16:01:49 +0530

    31080

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('items/', ItemView.as_view(), name='item-list'),
+    path('menu/', views.menu_list, name='menu_list'),
 ]
