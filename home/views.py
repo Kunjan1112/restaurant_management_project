610c2fde687142a2c483f3d9ec75dc6298468c2b
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-13 10:28:00 +0530

    31083

--- home/views.py
+++ home/views.py
@@ -10,7 +10,12 @@ def name(request):
 
 def index(request):
     restaurant_name = getattr(settings, "RESTAURANT_NAME", "My Restaurant")
-    return render(request,'home/home.html', {"restaurant_name":restaurant_name})
+    restaurant_phone = getattr(settings, "RESTAURANT_PHONE", "Not Available")
+    return render(request,'home/home.html', {
+        "restaurant_name":restaurant_name,
+        "restaurant_phone":restaurant_phone
+        }
+    )
 
 def 404_view(request):
     return render(request,'home/404.html')
@@ -19,10 +24,18 @@ def login_view(request):
     return render(request,'home/login.html')
 
 def about_restaurant(request):
-    return render(request,'home/about.html')
+    restaurant_name = getattr(settings, "RESTAURANT_NAME", "My Reataurant")
+    restaurant_phone = getattr(settings, "RESTAURANT_PHONE",  "Not Available")
+    return render(request,'home/about.html',{
+        "restaurant_name" : restaurant_name,
+        "restaurant_phone" : restaurant_phone
+    })
 
 def contact_view(request):
-    return render(request,'home/contact.html')
+    restaurant_phone = getattr(settings, "RESTAURANT_PHONE", "Not Available")
+    return render(request,'home/contact.html',{
+        "restaurant_phone" : restaurant_phone
+    })
     
 
 
