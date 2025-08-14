c07c4316d044083e0399b0a248e1b7f61e51b41b
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-14 17:01:59 +0530

    32060

--- home/forms.py
+++ home/forms.py
@@ -1,1 +1,15 @@
+from django import forms
+from .models import Restaurant 
 
+class RestaurantForm(forms.ModelForm):
+    class Meta:
+        model = Restaurant
+        fields = ['name']
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback 
+        fields = ['comments']
+        widgets = {
+            'comments' : forms.Textarea(attrs={'rows':4, 'placeholder':"Your thots to Write Here....."}),
+        }
