2ad108d05e456b10b87c9910e6c1cc4e1450c11c
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-14 16:58:40 +0530

    32060

--- home/models.py
+++ home/models.py
@@ -1,1 +1,8 @@
+from django.db import models
 
+class Feedback(models.Model):
+    comments = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comments[:20]
