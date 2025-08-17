32f901eb1f457a47b65419b4924038eff13fc5b8
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-17 17:19:16 +0530

    32891

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order
 
 # Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer_name', 'created_at')
+    list_filter = ('created_at',)
