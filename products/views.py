221e29668bf3c9ff78c2c30d415179bf660fabd7
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-17 17:04:33 +0530

    32892

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 def menu_list(request):
     menu_items = [
@@ -8,3 +10,13 @@ def menu_list(request):
     ]
 
     return render(request, 'products/menu_list.html', {'menu_items':menu_items})
+
+@api_view(['GET'])
+def menu_list_api(request):
+    menu_items = [
+        {'name': 'Margherita Pizza', 'description': 'Classic cheese and tomato pizza', 'price': 299},
+        {'name': 'Veggie Burger', 'description': 'Burger with fresh vegetables and cheese', 'price': 199},
+        {'name': 'Pasta Alfredo', 'description': 'Creamy Alfredo pasta with mushrooms', 'price':249},
+    ]
+
+    return Response(menu_items)
