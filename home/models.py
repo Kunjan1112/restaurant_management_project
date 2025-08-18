453fc1b7585d1357e0bb688f6e1cd29d9259dbee
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-18 17:11:58 +0530

    35224

--- home/models.py
+++ home/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+
+class Home(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-submitted_at']
+
+    def __str__(self):
+        return self.title
+
 class Feedback(models.Model):
     comments = models.TextField()
     submitted_at = models.DateTimeField(auto_now_add=True)
