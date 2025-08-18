b74c1f6a1cd6f8fef1a029c9aec8a64cd4ebaf43
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-18 09:30:54 +0530

    34575

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -135,3 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 RESTAURANT_NAME = "Delicious Bites"
 RESTAURANT_PHONE = "+91-98765-43210"
 RESTAURANT_HOURS = "Mon-Fri: 11am-9pm, Sat-Sun: 10am-10pm"
+RESTAURANT_ADDRESS = "123, Main Street, Ahemdabad, Gujarat, India"
+GOOGLE_MAP_EMBED_URL = "https://www.google.com/maps"
