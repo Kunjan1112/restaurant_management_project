8270ad1c209c9b30cf981c7a92a15edf8df71818
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-18 09:53:37 +0530

    34574

--- home/models.py
+++ home/models.py
@@ -6,3 +6,11 @@ class Feedback(models.Model):
 
     def __str__(self):
         return self.comments[:20]
+
+class ContactSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
