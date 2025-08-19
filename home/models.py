49553d512e1c70ddeddd797101e9b13727e2b917
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-19 17:45:40 +0530

    35845

--- home/models.py
+++ home/models.py
@@ -22,6 +22,7 @@ class Feedback(models.Model):
 class ContactSubmission(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+    message = models.TextField()
     submitted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self): 
