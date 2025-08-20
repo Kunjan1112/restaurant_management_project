98fccdffd709e42c26783caa05403d00e48b021e
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-20 15:40:40 +0530

    36325

--- home/views.py
+++ home/views.py
@@ -4,12 +4,15 @@ from django.db import DatabaseError
 
 from django.conf import settings
 from django.contrib import messages
-from django.core.mail import send_mail
+from django.core.mail import send_email
 
 from .models import Restaurant
 from .forms import ContactForm
 
 # Create your views here.
+
+def generate_breadcrumbs(*args):
+    return args
 
 def name(request):
     return HttpResponse("Hello My Name is Kunjan")
@@ -19,9 +22,10 @@ def index(request):
     restaurant_phone = getattr(settings, "RESTAURANT_PHONE", "Not Available")
     restaurant_hours = getattr(settings, "RESTAURANT_HOURS", "Mon-Fri: 11am-9pm, Sat-Sun: 10am-10pm")
     restaurant_address = getattr(settings, "RESTAURANT_ADDRESS", "123, Main Street, Ahmedabad, Gujarat, India")
+
     google_map_embed_url = getattr(
         settings,
-        "GOOGLE_MAP_EMBED_URL"
+        "GOOGLE_MAP_EMBED_URL",
         "#"
     )
 
@@ -45,22 +49,28 @@ def index(request):
         "google_map_embed_url" : google_map_embed_url,
         "form" : form,
         "success_message" : success_message,
+        "breadcrumbs" : breadcrumbs
         }
     )
 
 def handler404(request):
-    return render(request,'home/404.html', status=404)
+    breadcrumbs = generate_breadcrumbs(('Home','/'), ('404',None))
+    return render(request,'home/404.html', {'breadcrumbs':breadcrumbs}, status=404)
 
 def login_view(request):
-    return render(request,'home/login.html')
+    breadcrumbs = generate_breadcrumbs(('Home','/'), ('Login',None))
+    return render(request,'home/login.html', {'breadcrumbs':breadcrumbs})
 
 def about_restaurant(request):
     restaurant_name = getattr(settings, "RESTAURANT_NAME", "My Restaurant")
     restaurant_phone = getattr(settings, "RESTAURANT_PHONE",  "Not Available")
     restaurant_hours = getattr(settings, "RESTAURANT_HOURS", "Mon-Fri: 11am-9pm, Sat-Sun: 10am-10pm")
+    breadcrumbs = generate_breadcrumbs(('Home','/'),('About',None))
+
     return render(request,'home/about.html',{
         "restaurant_name" : restaurant_name,
-        "restaurant_phone" : restaurant_phone
+        "restaurant_phone" : restaurant_phone,
+        'breadcrumbs' : breadcrumbs
     })
 
 def contact_view(request):
@@ -86,21 +96,26 @@ def contact_view(request):
             return redirect("contact")
     else:
         form = ContactForm()
-    
-    return render(request, 'contact.html',{"form":form})
+
+    breadcrumbs = generate_breadcrumbs(('Home','/'), ('Contact',None))
+    return render(request, 'contact.html',{"form":form, "breadcrumbs":breadcrumbs})
 
 def reservations_view(request):
     restaurant_name = getattr(settings, "RESTAURANT_NAME", "My Restaurant")
     restaurant_phone = getattr(settings, "RESTAURANT_PHONE", "Not Available")
     restaurant_hours = getattr(settings, "RESTAURANT_HOURS", "Mon-Fri: 11am-9pm, Sat-Sun: 10am-10pm")
+    breadcrumbs = generate_breadcrumbs(('Home','/'), ('Reservations',None))
+
     return render(request, 'home/reservations.html',{
         "restaurant_name":restaurant_name,
-        "restaurant_phone":restaurant_phone
+        "restaurant_phone":restaurant_phone,
+        'breadcrumbs':breadcrumbs
     })
 
 def restaurant_list(request):
     try:
         restaurants = Restaurant.objects.all()
+        breadcrumbs = generate_breadcrumbs(('Home','/'), ('Restaurant',None))
         return render(request,'home/restaurant_list.html', {'restaurants':restaurants})
     except DatabaseError as e:
         print(f"Database error: {e}")
@@ -119,15 +134,19 @@ def add_to_cart(request, item_id):
 def homepage(request):
     cart = request.session.get("cart", {})
     cart_count = sum(cart.values())
+    breadcrumbs = generate_breadcrumbs(('Home',None))
     context = {
         'restaurant_name' : "My Awesome Restaurant",
         "cart_count" : cart_count,
+        'breadcrumbs':breadcrumbs
     }
 
     return render(request, "home/home.html", context)
 
 def faq_view(request):
-    return render(request, 'home/faq.html')
+    breadcrumbs = generate_breadcrumbs(('Home','/'),('FAQ',None))
+    return render(request, 'home/faq.html', {'breadcrumbs':breadcrumbs})
 
 def privacy_policy(request):
-    return render(request,'home/privacy.html')   
+    breadcrumbs = generate_breadcrumbs(('Home','/'),('Privacy Policy',None))
+    return render(request,'home/privacy.html', {'breadcrumds':breadcrumbs})   
