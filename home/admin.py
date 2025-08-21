869c84d1a9beef2805855ee40c67d023822c1483
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-21 16:01:12 +0530

    36863

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Special
 
 # Register your models here.
+
+
+@admin.register(Special):
+class SpecialAdmin(admin.ModelAdmin):
+    list_display = ('item_name', 'price', 'created_at')
