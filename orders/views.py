cbb7dd5144a6d080e924c7dfe4108dbc841199af
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-21 09:24:17 +0530

    36324

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def order_confirmation(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'order_confirmation.html', {"order":order})
