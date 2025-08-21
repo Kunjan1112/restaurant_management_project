120715f7875d2060a2378b0c519f6d989e5ef8ef
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-21 16:36:50 +0530

    37110

--- home/admin.py
+++ home/admin.py
@@ -4,6 +4,10 @@ from .models import Special
 # Register your models here.
 
 
-@admin.register(Special):
+@admin.register(Special)
 class SpecialAdmin(admin.ModelAdmin):
     list_display = ('item_name', 'price', 'created_at')
+
+@admin.register(OpeningHour)
+class OpeningHourAdmin(admin.ModelAdmin):
+    list_display = ("day","open_time", "close_time")
