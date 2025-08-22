ec28f23fcec810086474ec1348299ec3bafdda6e
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-22 10:11:04 +0530

    37472

--- products/models.py
+++ products/models.py
@@ -6,6 +6,7 @@ class MenuItems(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
