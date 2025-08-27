54f5e6f0e3f90a1c8fff4d45d8dbc7e0584bbffb
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-27 10:12:24 +0530

    39685

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('api/',include('home.urls')),
     path('api/accounts/',include('account.urls')),
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
+    
 ]
 
 handler403 = views.custom_permission_denied_view
