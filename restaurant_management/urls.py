2802ae6a06a22577198f543c55b59c05f4427626
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-08-27 10:18:19 +0530

    39685

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -30,5 +32,4 @@ urlpatterns = [
 handler403 = views.custom_permission_denied_view
 
 if settings.DEBUG:
-    from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
