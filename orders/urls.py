a8a356a6b097a6ca9abc999714eaecc705881ecf
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-09-01 09:28:40 +0530

    40390

--- orders/urls.py
+++ orders/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("order/confirmation/<int:order_id>/",views.order_confirmation,name="order_confirmation"),
+
+    path('thank-you/',views.thank_you,name="thank_you"),
 ]
