a867e52cc6a892a85535909ab2d48df92d81e42f
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-09-01 09:28:03 +0530

    40390

--- orders/views.py
+++ orders/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 def order_confirmation(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'order_confirmation.html', {"order":order})
+
+def thank_you(request):
+        return render(request, 'order/thank_you.html')
