6f6924c703ef453aff786c5fcb2bca320963d2aa
Kunjan1112 <123051804+Kunjan1112@users.noreply.github.com>
2025-09-03 11:04:35 +0530

    41725

--- home/models.py
+++ home/models.py
@@ -102,3 +102,11 @@ class RestaurantInfo(models.Model):
 
     def __str__(self):
         return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
