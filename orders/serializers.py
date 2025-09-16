dad018453718e81d5b5c1265d32057a38031de01
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-16 12:12:35 +0530

    45835

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,15 +1,21 @@
 from rest_framework import serializers
 from .models import Order, OrderItem
+from products.models import Menu
 
 
 class OrderItemSerializer(serializers.ModelSerializers):
+    menu_item_name = serializers.CharField(source='menu_item.name', read_only=True)
+    menu_item_price = serializers.DecimalField(source='menu_item.price', max_digits=8, decimal_places=2, read_only=True)
+
+
     class Meta:
         model = OrderItem
-        fields = ["product_name", "quantity", "price"]
+        fields = ["id", "menu_item_name", "menu_item_price", "quantity"]
 
 class OrderItemSerializer(serializers.ModelSerializers):
-    items = OrderItemSerializer(many=True, read_only=True)
+    customer_username = serializers.CharField(source='customer.username', read_only=True)
+    order_items = OrderItemSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
-        fields = ["id", "created_at", "total_price", "items"]
+        fields = ["id", "customer_username", "total_amount", "status", "created_at", "order_items"]
