0927e25c370d61b5878218cf118774ffbbb14306
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-16 11:38:10 +0530

    45837

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .serializers import UserProfileSerializer
 
 # Create your views here.
+
+class UserProfileUpdateView(generics.UpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permissions_classes = [permissions.IsAuthenticated]
+
+
+    def get_object(self):
+        return self.request.user
