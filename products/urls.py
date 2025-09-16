dadf7cdfd604d0c343853e90809217d588863373
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-16 11:14:45 +0530

    45678

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MenuCategoryView
-
-router = DefaultRouter()
-router.register(r"search", MenuSearchViewSet, basename="menu-search")
+from .views import MenuSearchViewSet
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path('menu/search/', MenuSearchViewSet.as_view({'get':'list'}), name='menu-search'),
 ]
