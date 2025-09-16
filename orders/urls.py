62da4f6db5f068cc5c5a3570bc3e82c5e90f4792
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-16 09:55:07 +0530

    45682

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("order/confirmation/<int:order_id>/",views.order_confirmation,name="order_confirmation"),
-    path('thank-you/',views.thank_you,name="thank_you"),
+    path('order-history/', views.OrderHistroyView, name="order-histroy")
 ]
