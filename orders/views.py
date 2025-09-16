a2f4a3a3a23ccca57f927565b9188d12e36bcd60
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-16 12:01:26 +0530

    45836

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Order
 from .serializers import OrderSerializers
+from utils.email_utils import send_order_confirmation_email
 
 class OrderHistroyView(APIView):
     permission_classes = [IsAuthenticated]
@@ -11,3 +12,14 @@ class OrderHistroyView(APIView):
         orders = Order.objects.filter(user=request.user).order_by("-created_at")
         serializer = OrderSerializers(orders, many=True)
         return Response(serializer.data)
+
+def confirm_order(order_id):
+    order = Order.objects.get(id=order_id)
+    result = send_order_confirmation_email(
+        order_id=order.id,
+        customer_email = order.customer.email,
+        customer_name = order.customer.username,
+        total_amount = order.total_amount
+    )
+
+    print(result)
