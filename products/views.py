72b45fa9f04a45d399e7ae1a76a4b0f2860f7109
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-16 11:10:58 +0530

    45678

--- products/views.py
+++ products/views.py
@@ -1,18 +1,32 @@
-from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from .models import Menu
-from .serializers import MenuSerializer
+from .serializers import MenuSerializer 
 
-class MenuViewSet(viewsets.ModelViewSet):
+
+class MenuPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class MenuSearchViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Menu.objects.all()
-    serializers_class = MenuSerializer
-    permissions_classes = [permissions.IsAdminUser]
+    serializer_class = MenuSerializer
+    permissions_classes = [permissions.AllowAny]
+    pagination_class = MenuPagination
 
-    def update(self, request, *args, **kwargs):
-        try:
-            return super().update(request, *args, **kwargs)
-        except Exception as e:
-            return Response(
-                {"error":str(e)},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        query = request.query_params.get("q")
+
+        if query:
+            queryset = queryset.filter(name_icontains=query)
+
+        page = self.pagination_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
