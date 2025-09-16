e76b75eb6d8a8c8f63a3eec93a1a0a89e49ec254
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-16 11:12:32 +0530

    45678

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MenuCategoryView
 
-# router = DefaultRouter()
-# router.register(r"menu-items", MenuViewSet, basename="menu")
+router = DefaultRouter()
+router.register(r"search", MenuSearchViewSet, basename="menu-search")
 
 urlpatterns = [
-    path("items/", MenuCategoryView.as_view(), name="menu-by-cagtegory"),
+    path("", include(router.urls)),
 ]
