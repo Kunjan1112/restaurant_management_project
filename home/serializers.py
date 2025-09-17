00640393852ab1bc0531292c1d99f73d73671765
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-17 17:24:03 +0530

    47756

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from products.models import MenuCategory
+from .models import ContactFormSubmission
 
 class MenuCategorySerializer(serializers.ModelSerializers):
     class Meta:
         model = MenuCategory
         fields = ['id', 'name']
+
+class ContactFormSubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactFormSubmission
+        fields = ['id', 'name', 'email', 'message', 'created_at']
+        
