ad79bbea752b46699f0ae22d0d06909bb085b116
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-17 17:08:22 +0530

    47757

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+
 from .models import Order
 from .serializers import OrderSerializers
 from utils.email_utils import send_order_confirmation_email
-from rest_framework import status, viewsets
-from rest_framework.decorators import Response
 
 class OrderHistroyView(APIView):
     permission_classes = [IsAuthenticated]
@@ -16,15 +17,17 @@ class OrderHistroyView(APIView):
         return Response(serializer.data)
 
 def confirm_order(order_id):
-    order = Order.objects.get(id=order_id)
-    result = send_order_confirmation_email(
-        order_id=order.id,
-        customer_email = order.customer.email,
-        customer_name = order.customer.username,
-        total_amount = order.total_amount
-    )
-
-    print(result)
+    try:
+        order = Order.objects.get(id=order_id)
+        result = send_order_confirmation_email(
+            order_id=order.id,
+            customer_email = order.customer.email,
+            customer_name = order.customer.username,
+            total_amount = order.total_amount
+        )
+        print(result)
+    except Order.DoesNotExist:
+        print(f"Order with ID {order_id} not found.")
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
