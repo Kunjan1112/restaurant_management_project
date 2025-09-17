7c5700471d4bb867b2dc8eae30c95ce0751d26e6
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-17 17:17:36 +0530

    47757

--- orders/views.py
+++ orders/views.py
@@ -14,6 +14,7 @@ class OrderHistroyView(APIView):
     def get(self, request):
         orders = Order.objects.filter(user=request.user).order_by("-created_at")
         serializer = OrderSerializers(orders, many=True)
+        serializer_class = OrderSerializer
         return Response(serializer.data)
 
     def post(self, request):
