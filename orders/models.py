ac1a7ec3457dc9cc77882c75ebade08d3c565608
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-17 16:19:00 +0530

    47759

--- orders/models.py
+++ orders/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 
 from products.models import Menu
 
+from home.models import MenuItem
+
 # Create your models here.
 
 class ActiveOrderManger(models.Manager):
