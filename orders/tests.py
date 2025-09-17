26bb385b73815ff139fcd77128f1d501aef0194d
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-17 16:26:11 +0530

    47759

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from home.models import MenuItem 
+from orders.models import Order, OrderItem 
 
 # Create your tests here.
+
+class OrderModelTest(TestCase):
+    def setUp(self):
+        pizza = MenuItem.objects.create(name="Pizza", price=200.00)   
+        burger = MenuItem.objects.create(name="Burger", price=100.00)
+
+        self.order = Order.objects.create()
+
+        OrderItem.objects.create(order=self.order, menu_item=pizza, quantity=2, price=pizza.price)
+        OrderItem.objects.create(order=self.order, menu_item=burger, quantity=3, price=burger.price)
+
+    def test_calculate_total(self):
+        total = self.order.calculate_total()
+        self.assertEqual(total, 2*200.00 + 3*100.00) 
