73d9b7c6bf4bcfd49726ec2f81957dcc8e56b41c
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-17 17:41:01 +0530

    47755

--- orders/utils.py
+++ orders/utils.py
@@ -3,6 +3,11 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
 
+import string
+import secrets
+from .models import Order
+
+
 def send_email(recipient, subject, message):
     try:
         validate_email(recipient)
@@ -28,3 +33,11 @@ def send_email(recipient, subject, message):
     except Exception as e:
         print(f"Error sending email: {str(e)}")
         return False
+
+def generate_unique_order_id(length_0):
+    characters = string.ascii_uppercase + string.digits 
+
+    while True:
+        order_id = "".join(secrets.choice(characters) for _ in range(length))
+        if not Order.objects.filter(order_id=order_id).exists():
+            return order_id
