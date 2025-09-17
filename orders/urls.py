aa23011dbce43d5d6b5ee287f9d28beb071e9438
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-17 16:48:45 +0530

    47758

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import OrderHistroyView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import OrderHistroyView, OrderViewSet
+
+router = DefaultRouter()
+router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('history/', OrderHistroyView.as_view(), name="order-histroy"),
+
+    path('',include(router.urls)),
 ]
