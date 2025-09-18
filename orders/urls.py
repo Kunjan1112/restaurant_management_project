26dc1856989bb789794686ef740767fe3f50132e
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-18 11:42:10 +0530

    48336

--- orders/urls.py
+++ orders/urls.py
@@ -6,11 +6,9 @@ router = DefaultRouter()
 router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
-    
     path('history/', OrderHistroyView.as_view(), name="order-histroy"),
 
     path('',include(router.urls)),
 
     path('update-status/', UpdateOrderStatusView.as_view(), name="update-order-status"),
-    
 ]
