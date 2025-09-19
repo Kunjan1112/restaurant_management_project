87712fefb78df8efe547d014ced7fc54e220d7f4
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-19 11:12:52 +0530

    49197

--- home/models.py
+++ home/models.py
@@ -116,6 +116,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
+    is_available = models.BooleanField(default=True)
     category = models.CharField(
         max_length = 20,
         choices = CATEGORY_CHOICES,
