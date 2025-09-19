b0ee4268871df98e05a2b8cc979be93fdaf45e49
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-19 09:27:26 +0530

    48440

--- home/serializers.py
+++ home/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from products.models import MenuCategory
 
-from .models import ContactFormSubmission, MenuItem 
+from .models import ContactFormSubmission, MenuItem, UserReview
 
 class MenuCategorySerializer(serializers.ModelSerializer):
 
@@ -11,6 +11,8 @@ class MenuCategorySerializer(serializers.ModelSerializer):
         model = MenuCategory
         fields = ['id', 'name']
 
+# -----------------------------------------------------------------------------------------
+
 class ContactFormSubmissionSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -18,9 +20,25 @@ class ContactFormSubmissionSerializer(serializers.ModelSerializer):
         model = ContactFormSubmission
         fields = ['id', 'name', 'email', 'message', 'created_at'] 
         
+# -----------------------------------------------------------------------------------------
+
 class MenuItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         
         model = MenuItem
         fields = ["id", "name", "description", "price", "category", "is_daily_special"]
+
+# -----------------------------------------------------------------------------------------
+
+class UserReviewSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source="user.username")
+
+    class Meta:
+        model = UserReview
+        fields = ['id', 'user', 'menu_item', 'rating', 'comment', 'review_date']
+
+    def validate_rating(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Rating must be between 1 and 5.")
+        return value
