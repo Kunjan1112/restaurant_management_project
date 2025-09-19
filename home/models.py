3cfeaa8618bce97e234e935b7354544c791a00de
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-19 10:42:44 +0530

    49198

--- home/models.py
+++ home/models.py
@@ -196,3 +196,14 @@ class UserReview(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.menu_item.name} ({self.rating}/5)"
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=20)
+    opening_hours = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
