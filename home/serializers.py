79e3f1c2403eee9d07a5f5ea374558b960bd51fe
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-19 09:00:23 +0530

    48442

--- home/serializers.py
+++ home/serializers.py
@@ -3,16 +3,22 @@ from products.models import MenuCategory
 from .models import ContactFormSubmission, MenuItem
 
 class MenuCategorySerializer(serializers.ModelSerializers):
+
     class Meta:
+
         model = MenuCategory
         fields = ['id', 'name']
 
 class ContactFormSubmissionSerializer(serializers.ModelSerializer):
+
     class Meta:
+
         model = ContactFormSubmission
         fields = ['id', 'name', 'email', 'message', 'created_at'] 
         
 class MenuItemSerializer(serializers.ModelSerializer):
+
     class Meta:
+        
         model = MenuItem
         fields = ["id", "name", "description", "price", "category", "is_daily_special"]
