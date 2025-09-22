93abbad8c07e26ca71db548fa1576bd8f1c39eda
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-22 11:25:52 +0530

    50256

--- orders/urls.py
+++ orders/urls.py
@@ -8,9 +8,9 @@ router.register(r'orders', OrderViewSet, basename='order')
 urlpatterns = [
     path('history/', OrderHistroyView.as_view(), name="order-histroy"),
 
-    path('',include(router.urls)),
+    path('api/',include(router.urls)),
 
-    path('update-status/', UpdateOrderStatusView.as_view(), name="update-order-status"),
+    path('api/update-status/', UpdateOrderStatusView.as_view(), name="update-order-status"),
 
     path('api/order/<str:unique_id>/status/', OrderStatusRetrieveAPIView.as_view(), name='order-status'),
 ]
