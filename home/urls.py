d83202bc69ff18ed62079415d2ac9186eb98a560
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-22 11:01:58 +0530

    50257

--- home/urls.py
+++ home/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
     path('api/reviews/create/', views.UserReviewCreateView.as_view(), name="create-review"),
     path('api/review/menu-item/<int:menu_item_id>/', views.MenuItemReviewListView.as_view(), name="menu-item-reviews"),
     path('api/restaurant/', RestaurantDetailsAPIView.as_view(), name="restaurant-detail"),
+    path('api/menu-items/', MenuItemListAPIView.as_view(), name='menu-items-list'),
 
 
     path('orders/<int:order_id>/status/', views.get_order_status, name="get_order_status"),
