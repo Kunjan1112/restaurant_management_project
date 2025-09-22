0d9422dd5ebd6c2a2f388bcbe7487b3fb7d0ead5
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-22 10:44:25 +0530

    50258

--- orders/models.py
+++ orders/models.py
@@ -104,13 +104,14 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
-    menu_item = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=200)
     quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"{self.quantity} x {self.menu_item.name}"
-
+        return f"{self.product_name} X {self.quantity}"
+ 
 class Coupon(models.Model):
     code = models.CharField(max_length=20, unique=True)
     discount_precent = models.PositiveIntegerField(default=0)
