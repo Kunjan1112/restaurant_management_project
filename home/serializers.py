26b038ce0853197b3d1f51d9915162092e1446e4
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-23 09:19:53 +0530

    50362

--- home/serializers.py
+++ home/serializers.py
@@ -58,3 +58,15 @@ class MenuItemAvailabilitySerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = ['id', 'is_available']
+
+# -------------------------------------------------ReviewSerializer---------------------------------------
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'user', 'rating', 'text', 'created_at']
+
+        def validate_rating(self, value):
+            if value < 1 or value > 5:
+                raise serializers.ValidationError("Rating must be between 1 and 5.")
+            return value
