7ef3083fbd17be724128a303a8d8eaa65e4c1bf6
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-23 09:55:07 +0530

    50358

--- home/urls.py
+++ home/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
 
     path('orders/<int:order_id>/status/', views.get_order_status, name="get_order_status"),
     path('menu/<int:pk>/availability/', update_menu_item_availability, name='update-menu-availability'),
+    path('restatuant/opening-hours/', OpeningHoursView.as_view(), name='opening-hours'),
     
 
     path('api/', include(router.urls)),
