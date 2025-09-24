612d74c36771ae4709eb4e9beddc836289e2b329
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-24 09:06:47 +0530

    50595

--- home/models.py
+++ home/models.py
@@ -227,13 +227,14 @@ class UserReview(models.Model):
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     address = models.TextField()
-    phone_number = models.CharField(max_length=20)
-    opening_hours = models.CharField(max_length=255, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+    def get_total_menu_items(self):
+        return self.menu_items.count()
 
 # -----------------------------------------Reservation--------------------------------------------
 
