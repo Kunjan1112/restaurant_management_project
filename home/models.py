cb894f891493eb723f348618be12bba181ffa318
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-25 17:47:38 +0530

    51317

--- home/models.py
+++ home/models.py
@@ -254,3 +254,13 @@ class Reservation(models.Model):
     def __str__(self):
         return f"{self.customer_name} - {self.reservation_date} {self.start_time}-{self.end_time}" 
 
+# -------------------------------------------FAQ---------------------------------------------
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
+
