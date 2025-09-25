b7919fecf065c5aafbed2e8c0b301e13c693c09f
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-25 17:08:50 +0530

    50922

--- products/models.py
+++ products/models.py
@@ -5,9 +5,11 @@ class MenuItems(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+        
 # -------------------------------------------------------------------------------------
 
 class MenuCategory(models.Model):
