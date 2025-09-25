eeb9688b45dc947f36d00c0dcef34cf0bc2a23f7
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-25 16:49:37 +0530

    50925

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MenuSearchViewSet, MenuCategoryListAPIView
+from .views import MenuSearchViewSet, MenuCategoryListAPIView, MenuItemPriceRangeView
 
 urlpatterns = [
     path('menu/search/', MenuSearchViewSet.as_view({'get':'list'}), name='menu-search'),
 
     path('api/menu-categories/', MenuCategoryListAPIView.as_view(), name='menu-categories-list'),
+
+    path('menu-items/price-range/', MenuItemPriceRangeView.as_view(), name="menu-items-price-range"),
 ]
