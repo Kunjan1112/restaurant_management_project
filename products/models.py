e3c15bb597bdd633e2dcc52aa4e17e05c4a160e4
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-25 16:35:54 +0530

    50925

--- products/models.py
+++ products/models.py
@@ -3,10 +3,8 @@ from django.db import models
 # Create your models here.
 class MenuItems(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
-    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
