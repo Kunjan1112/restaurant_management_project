bdd7f70d1a8e01a54a640cc2bbdc5f371a98b6fd
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-25 17:01:08 +0530

    50924

--- home/models.py
+++ home/models.py
@@ -177,13 +177,13 @@ NEWS_ITEMS = [
 # ------------------------------------------------Review---------------------------------
 
 class Review(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
-    rating = models.PositiveIntegerField() 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.rating} Stars"
+        return f"Review {self.id} - {self.rating} stars"
 
 # --------------------------------------------RestaurantReview-------------------------------
 
