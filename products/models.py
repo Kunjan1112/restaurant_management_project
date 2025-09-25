4e3311f7a0b8ccb6b721bf953a3c9be37a77f9ef
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-25 17:16:57 +0530

    50921

--- products/models.py
+++ products/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+# ------------------------------------MenuItems---------------------------------
+
 # Create your models here.
 class MenuItems(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     is_available = models.BooleanField(default=True)
+    cuisine_type = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.name
