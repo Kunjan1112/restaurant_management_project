a14d8a255ec682a47ecdef55b52bbea3197766cd
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-26 17:24:38 +0530

    51682

--- home/models.py
+++ home/models.py
@@ -102,26 +102,23 @@ class RestaurantInfo(models.Model):
     def __str__(self):
         return self.name
 
+# -----------------------------------------MenuItem------------------------------------------
+
 class MenuItem(models.Model):
-    CATEGORY_CHOICES = [
-        ('appetizer', 'Appetizer'),
-        ('main_course', 'Main Course'),
-        ('dessert', 'Dessert'),
-        ('beverage', 'Beverage'),
-
-    ]
-
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=7, decimal_places=2)
-    category = models.CharField(
-        max_length = 20,
-        choices = CATEGORY_CHOICES,
-        default = 'main_course'
-    )
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    cuisine = models.CharField(max_length=100)
+    available = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.name} ({self.get_category_display()})"
+        return self.name
+
+    @classmethod
+    def get_items_by_cuisine(cls, cuisine_type:str):
+
+        return cls.objects.filter(cuisine__iexact=cuisine_type)
+
+# -----------------------------------------------ContactInfo---------------------------------------------
 
 class ContactInfo(models.Model):
     address = models.TextField()
