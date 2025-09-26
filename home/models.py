0ccd7f5a3df748d3a8645cda1bfe879f860ac51f
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-09-26 17:32:42 +0530

    51681

--- home/models.py
+++ home/models.py
@@ -108,7 +108,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     cuisine = models.CharField(max_length=100)
-    available = models.BooleanField(default=True)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
