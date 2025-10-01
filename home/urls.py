d8084c3b8caf5246763e89ecd0cf0add10fe02f0
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-10-01 10:35:25 +0530

    51687

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from rest_framework.rounters import DefaultRouter
+from rest_framework.routers import DefaultRouter
 from .views import MenuItemViewSet
 
 router = DefaultRouter()
