d632bca019fac7494206db5cf2a321263307cb65
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-10-01 10:51:02 +0530

    51685

--- home/models.py
+++ home/models.py
@@ -126,6 +126,7 @@ class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     class Meta:
+        
         verbose_name_plural = 'Menu Categories'
 
     def __str__(self):
