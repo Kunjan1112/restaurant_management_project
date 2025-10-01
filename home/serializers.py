4fb26cb8a7ecc0abdbd62a84b44f5afa4de4861a
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-10-01 10:46:43 +0530

    51686

--- home/serializers.py
+++ home/serializers.py
@@ -10,7 +10,7 @@ class MenuCategorySerializer(serializers.ModelSerializer):
 
     class Meta:
 
-        model = MenuCategory
+        model = MenuCategory 
         fields = ['id', 'name', 'description']
 
 # -----------------------------------------------------------------------------------------
