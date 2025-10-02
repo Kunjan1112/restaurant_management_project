9663a8368f611f46bb3b278ed64e843ae53a8ffb
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-10-02 21:21:53 +0530

    53207

--- home/utils.py
+++ home/utils.py
@@ -8,19 +8,19 @@ from .models import Table
 def calculate_discount(price, discount_percentage):
 
     try:
-        original_price = float(original_price)
+        price = float(price)
         discount_percentage = float(discount_percentage)
 
-        if original_price < 0:
-            raise ValueError("Original price cannot be negative.")
-        if not(0 <= discount_percentage <= 100):
+        if price < 0:
+            raise ValueError("Price cannot be nagetive.")
+        if not (0 <= discount_percentage <= 100):
             raise ValueError("Discount percentage must be between 0 and 100.")
 
-        discount_amount = (discount_percentage / 100) * original_price
-        discount_price = original_price - discount_amount
+        discount_amount = (discount_percentage / 100) * price
+        discounted_price = price - discount_amount
 
-        return round(discount_price, 2)
-    
+        return round(discounted_price, 2)
+
     except (ValueError, TypeError) as e:
         print(f"Error calculating discount: {e}")
         return None
