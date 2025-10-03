8a0bedce1c6f475afa8a0fdbfc4aa673156594b4
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-10-03 09:47:31 +0530

    53206

--- home/models.py
+++ home/models.py
@@ -135,6 +135,7 @@ class MenuCategory(models.Model):
 # -----------------------------------------MenuItem------------------------------------------
 
 class MenuItem(models.Model):
+    
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
