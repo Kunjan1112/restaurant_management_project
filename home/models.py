57261e1824d971685a7c939a4a9f115169d1ce6b
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-10-07 09:09:54 +0530

    53870

--- home/models.py
+++ home/models.py
@@ -135,18 +135,24 @@ class MenuCategory(models.Model):
 
 # -----------------------------------------MenuItem------------------------------------------
 
-class MenuItem(models.Model):
-    
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='menu_items')
-    allergens = models.TextField(blank=True, null=True, help_text="Comma-separated list of allerges (e.g., glutes, nuts, dairy)")
-    
+class Cuisine(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        if self.allergens:
-            return f"{self.name} (Allergens: {self.allergens})"
+        return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    cuisine = models.ForeignKey(
+        Cuisine,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='menu_items'
+    )
+
+    def __str__(self):
         return self.name
 
 # -----------------------------------------------ContactInfo---------------------------------------------
