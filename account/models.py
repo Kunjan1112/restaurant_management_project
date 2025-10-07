bc1b450f3e1d2e050791fb5200309be13cc059f3
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-10-07 09:42:10 +0530

    53867

--- account/models.py
+++ account/models.py
@@ -1,11 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+PREFERRED_CUISINE_CHOICES = (
+    ('Italian', 'Italian'),
+    ('Mexican', 'Mexican'),
+    ('Asian', 'Asian'),
+    ('Vegetarian', 'Vegetarian'),
+)
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    preferred_cuisine = models.CharField(
+        max_length = 50,
+        choices = PREFERRED_CUISINE_CHOICES,
+        null = True,
+        blank = True
+    )
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user.username}'s Profile"
+
+@receiver(post_save, sender=user)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
