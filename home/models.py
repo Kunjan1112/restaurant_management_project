3b1795cda9ecae392283afe15d92186beddaa157
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-10-07 09:25:05 +0530

    53868

--- home/models.py
+++ home/models.py
@@ -232,7 +232,8 @@ class Table(models.Model):
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    capacity = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} (Capacity: {self.capacity})"
         
