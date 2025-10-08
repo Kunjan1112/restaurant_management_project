4917dadf2e29feac633927b7327d6b6479b31196
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-10-08 08:39:41 +0530

    54155

--- home/models.py
+++ home/models.py
@@ -139,7 +139,7 @@ class Cuisine(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return self.name
+        return self.name 
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=200)
