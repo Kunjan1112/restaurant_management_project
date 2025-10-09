fb0a58dcc415717c5a77c6870728f82f8e61a600
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-10-09 20:58:50 +0530

    54476

--- account/models.py
+++ account/models.py
@@ -30,3 +30,13 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+# ------------------------------------------------CustomerProfile----------------------------------------------
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(settings.Auth_USER_MODEL, on_delete=models.CASCADE, related_name='customer_profile')
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    delivery_address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
