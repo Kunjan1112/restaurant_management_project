7ee493355ff7f4a61843d0c6e8cd355a9f86757b
Kunjan Patel <123051804+Kunjan1112@users.noreply.github.com>
2025-10-17 10:36:14 +0530

    54474

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Table
+from .models import Table, DailyOperatingHours
 
 # Register the Table model
 
 admin.site.register(Table)
+
+admin.site.register(DailyOperatingHours)
 
